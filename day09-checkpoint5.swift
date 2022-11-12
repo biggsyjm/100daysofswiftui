05545ab836c63d73f7259057835aa98abf3d4478
biggsyjm <118052872+biggsyjm@users.noreply.github.com>
2022-11-12 15:03:50 -0500

    Create day09-checkpoint5.swift

---
+++ day09-checkpoint5.swift
@@ -0,0 +1,8 @@
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let sortedLuckyNumbers = { (numbers: [Int]) in
+    numbers.sorted()
+        .filter { !$0.isMultiple(of: 2) }
+        .map { print("\($0) is a lucky number")}
+}
+sortedLuckyNumbers(luckyNumbers)
