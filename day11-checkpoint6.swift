f022daf790ac46c3f463c9f979d421f6306540bc
biggsyjm <118052872+biggsyjm@users.noreply.github.com>
2022-11-14 06:20:50 -0500

    Create day11-checkpoint6.swift

---
+++ day11-checkpoint6.swift
@@ -0,0 +1,52 @@
+struct Car {
+    let model: String
+    let seats: Int
+    private var gear = 1
+    init(model: String, seats: Int) {
+           self.model = model
+           self.seats = seats
+       }
+    mutating func changeGear(up: Bool) {
+        if up {
+            if gear <= 9 {
+                gear += 1
+                print("Changed to \(gear) gear")
+            }
+            if gear > 9 {
+                print("Gear can't go any higher")
+            }
+        }
+        else if !up {
+            if gear > 1 {
+                gear -= 1
+                print("Lowered to \(gear) gear")
+            }
+            if gear == 1 {
+                print("Gear can't go any lower")
+            }
+        }
+    }
+}
+
+var honda = Car(model: "Odyssey", seats: 9)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: true)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
+honda.changeGear(up: false)
