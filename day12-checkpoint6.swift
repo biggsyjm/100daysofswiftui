926ac125c5b76d4cedebad95a60d3e2edcf3f47f
biggsyjm <118052872+biggsyjm@users.noreply.github.com>
2022-11-15 06:29:36 -0500

    Create day12-checkpoint6.swift

---
+++ day12-checkpoint6.swift
@@ -0,0 +1,38 @@
+//day 12 checkpoint 6
+
+class Animal {
+    let legs: Int
+    init(legs: Int) {
+        self.legs = legs}}
+class Dog: Animal {
+    func speak() {
+            print("woof woof bow wow wow")}}
+class Corgi: Dog {
+    override func speak() {
+        print("yap yap yap yap")}}
+class Poodle: Dog {
+    override func speak() {
+        print("arf arf arf arf ")}}
+
+class Cat: Animal {
+    var isTame: Bool
+    func speak() {
+            print("meow meow meow")}
+    init(isTame: Bool) {
+        self.isTame = isTame
+        super.init(legs: 4)}}
+class Persian: Cat {
+    override func speak() {
+        print("meooooooooooooow")}}
+class Lion: Cat {
+    override func speak() {
+        print("ROOOAAAARRRRR")}}
+
+let brandy = Corgi(legs: 4)
+brandy.speak()
+let oscar = Poodle(legs: 4)
+oscar.speak()
+let sansa = Persian(isTame: true)
+sansa.speak()
+let simba = Lion(isTame: false)
+simba.speak()
